ea9a358e8e136ac3d2abdd00e2b64a95c5e194b3
rocksandy06 <149867592+rocksandy06@users.noreply.github.com>
2023-11-17 20:23:22 +0530

    OOPS_Inheritance

---
+++ src/OOPS_Inheritance/Main.java
@@ -0,0 +1,13 @@
+package OOPS_Inheritance;
+
+
+public class Main {
+    
+    	public static void main(String args[])
+        {
+            Son o =new Son();
+            o.car();
+            o.house();
+            System.out.println();
+    	    }
+    	}
---
+++ src/OOPS_Inheritance/Son.java
@@ -0,0 +1,28 @@
+package OOPS_Inheritance;
+
+
+
+public class Son {
+	class Father //Base
+	{
+	    public void house()
+	    {
+	        System.out.println("Have Own 2BHK House.");
+	    }
+	}
+	class son extends Father //Derived
+	{
+	    public void car()
+	    {
+	        System.out.println("Have Own Audi Car.");
+	    }
+	}
+	public void car() {
+		// TODO Auto-generated method stub
+		
+	}
+	public void house() {
+		// TODO Auto-generated method stub
+		
+	}
+}
